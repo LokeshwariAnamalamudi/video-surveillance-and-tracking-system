3cf1347273e75f376f77152a70dc4a3b51e38eaa
LokeshwariAnamalamudi <126390841+LokeshwariAnamalamudi@users.noreply.github.com>
2023-10-24 12:41:50 +0530

    Add files via upload

---
+++ data_handling.py
@@ -0,0 +1,49 @@
+import sqlite3 as sl
+con = sl.connect('data.db')
+
+
+# with con:
+#     con.execute("""
+#         CREATE TABLE USER (
+#             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+#             name TEXT,
+#             location TEXT,
+#             time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#         );
+#     """)
+
+
+# with con:
+#     con.execute("""
+#         DROP TABLE USER;
+#     """)
+
+# with con:
+#     con.execute("""
+#         Delete FROM USER;
+#     """)
+
+# sql = 'INSERT INTO USER (name, location) values(?, ?)'
+# data = [
+#     ('Apoorv Mishra', 'Main Gate'),
+#     ('Arpit Mishra', 'Main Gate'),
+#     ('Mahima', 'Main Gate')
+# ]
+# with con:
+#     con.executemany(sql, data)
+
+with con:
+    data = con.execute("SELECT * FROM USER")
+    for row in data:
+        print(row)
+
+
+# query="UPDATE USER SET location=?, time= ? "
+# data=('Hostel M', time.asctime( time.localtime(time.time()) ))
+#     with con:
+#         con.execute(query, data)
+
+# with con:
+#     con.execute("""
+#         UPDATE USER SET name='disy' WHERE id=460;
+#     """)
